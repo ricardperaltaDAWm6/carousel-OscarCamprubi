af6f27c95651c3dd4f047cffb5cf6544ddcc7e24
OscarCamprubi <oscar.camprubi@cirvianum.cat>
2021-11-17 15:23:46 +0100

    Carousel Final

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
     // Variables
 
     // Añadir las tres imágenes del directorio "img" al array IMAGENES.
-    const IMAGENES = [];
+    const IMAGENES = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg"];
 
     const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
 
@@ -10,11 +10,12 @@ window.onload = function () {
     let posicionActual = 0;
 
     // variables con los elementos del DOM HTML, aplicar el selector necesario.
-    let $botonRetroceder
-    let $botonAvanzar 
-    let $imagen 
-    let $botonPlay 
-    let $botonStop
+    let $botonRetroceder = document.getElementById("retroceder");
+    let $botonAvanzar = document.getElementById("avanzar");
+    let $imagen = document.getElementById("imagen");
+    let $botonPlay = document.getElementById("play");
+    let $botonStop = document.getElementById("stop");
+
 
     // Identificador del proceso que se ejecuta con setInterval().
     let intervalo;
@@ -26,8 +27,13 @@ window.onload = function () {
      */
     function pasarFoto() {
         // se incrementa el indice (posicionActual)
+        posicionActual++;
+        if (posicionActual === IMAGENES.length) {
+            posicionActual = 0;
+        }
 
         // ...y se muestra la imagen que toca.
+        renderizarImagen();
     }
 
     /**
@@ -35,8 +41,13 @@ window.onload = function () {
      */
     function retrocederFoto() {
         // se incrementa el indice (posicionActual)
+        posicionActual--;
+        if (posicionActual === -1) {
+            posicionActual = IMAGENES.length - 1;
+        }
 
         // ...y se muestra la imagen que toca.
+        renderizarImagen();
     }
 
     /**
@@ -52,9 +63,14 @@ window.onload = function () {
     function playIntervalo() {
         // Documentación de la función setInterval: https://developer.mozilla.org/en-US/docs/Web/API/setInterval
         // Mediante la función setInterval() se ejecuta la función pasarFoto cada TIEMPO_INTERVALO_MILESIMAS_SEG.
-        
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
 
         // Desactivamos los botones de control necesarios. Utilizando setAttribute y removeAttribute.
+        $botonPlay.setAttribute("disabled", "");
+        $botonStop.removeAttribute("disabled");
+        $botonAvanzar.setAttribute("disabled", "");
+        $botonRetroceder.setAttribute("disabled", "");
+
 
     }
 
@@ -63,13 +79,49 @@ window.onload = function () {
      */
     function stopIntervalo() {
         // Desactivar la ejecución de intervalo.
+        clearInterval(intervalo);
 
         // Activamos los botones de control. Utilizando setAttribute y removeAttribute.
+        $botonPlay.removeAttribute("disabled");
+        $botonStop.setAttribute("disabled", "");
+        $botonAvanzar.removeAttribute("disabled");
+        $botonRetroceder.removeAttribute("disabled");
     }
 
+    function showInfoImagen() {
+        $imagen.innerText = `url(${IMAGENES[posicionActual]})`;
+    }
+
+    function leaveInfoImagen() {
+        $imagen.innerText = "";
+    }
+
+
     // Eventos
-    // Añadimos los evenntos necesarios para cada boton. Mediante addEventListener.
+    // Añadimos los eventos necesarios para cada boton. Mediante addEventListener.
+
+    $botonStop.addEventListener("click", stopIntervalo);
+    $botonPlay.addEventListener("click", playIntervalo);
+    $botonAvanzar.addEventListener("click", pasarFoto);
+    $botonRetroceder.addEventListener("click", retrocederFoto);
+    document.addEventListener('keydown', function (e) {
+        switch (e.keyCode) {
+            //Boton de la Izquierda
+            case 37:
+                retrocederFoto();
+                break;
+            //Boton de la Derecha
+            case 39:
+                pasarFoto();
+                break;
+        }
+    });
+    $imagen.addEventListener("mouseover",showInfoImagen);
+    $imagen.addEventListener("mouseleave",leaveInfoImagen);
+
 
     // Iniciar
     renderizarImagen();
+
+
 } 
